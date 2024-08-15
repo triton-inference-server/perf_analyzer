30550e2cd91230f86baa5886bffbdd6ff17d3790
lkomali <lkomali@nvidia.com>
2024-08-14 23:37:40 -0700

    Remove print statement

--- genai-perf/genai_perf/wrapper.py
+++ genai-perf/genai_perf/wrapper.py
@@ -171,4 +171,3 @@ class Profiler:
         finally:
             if telemetry_data_collector is not None:
                 telemetry_data_collector.stop()
-                print(telemetry_data_collector.metrics)
