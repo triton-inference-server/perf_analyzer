f461c83551b32bf1d3147f6f93948f9a294e8f5f
lkomali <lkomali@nvidia.com>
2024-08-27 14:56:56 -0700

    Fix comments

--- genai-perf/tests/test_cli.py
+++ genai-perf/tests/test_cli.py
@@ -918,19 +918,10 @@ class TestCLIArguments:
         actual_dict = parser.get_extra_inputs_as_dict(namespace)
         assert actual_dict == expected_dict
 
-    TEST_TRITON_METRICS_URL = "http://custom-metrics-url:8002/metrics"
-    INVALID_URL = "invalid_url"
-    INVALID_URL_ERROR_MESSAGE = (
-        "The URL passed for --server-metrics-url is invalid. "
-        "It must use 'http' or 'https', have a valid domain and port, "
-        "and contain '/metrics' in the path. The expected structure is: "
-        "<scheme>://<netloc>/<path>;<params>?<query>#<fragment>"
-    )
-
     @pytest.mark.parametrize(
-        "args_list, expected_url, expected_error",
+        "args_list, expected_url",
         [
-            # Test with a custom URL
+            # server-metrics-url is specified
             (
                 [
                     "genai-perf",
@@ -940,12 +931,11 @@ class TestCLIArguments:
                     "--service-kind",
                     "triton",
                     "--server-metrics-url",
-                    TEST_TRITON_METRICS_URL,
+                    "http://test-metrics-url:8002/metrics",
                 ],
-                TEST_TRITON_METRICS_URL,
-                None,
+                "http://test-metrics-url:8002/metrics",
             ),
-            # Test with default URL
+            # server-metrics-url is not specified
             (
                 [
                     "genai-perf",
@@ -956,36 +946,44 @@ class TestCLIArguments:
                     "triton",
                 ],
                 None,
-                None,
-            ),
-            # Test with invalid URL
-            (
-                [
-                    "genai-perf",
-                    "profile",
-                    "--model",
-                    "test_model",
-                    "--service-kind",
-                    "triton",
-                    "--server-metrics-url",
-                    INVALID_URL,
-                ],
-                None,
-                INVALID_URL_ERROR_MESSAGE,
             ),
         ],
     )
-    def test_server_metrics_url_for_triton(
-        self, args_list, expected_url, expected_error, monkeypatch, capsys
+    def test_server_metrics_url_valid_args(self, args_list, expected_url, monkeypatch):
+        monkeypatch.setattr("sys.argv", args_list)
+        args, _ = parser.parse_args()
+        assert args.server_metrics_url == expected_url
+
+    @pytest.mark.parametrize(
+        "args_list, expected_error",
+        [
+            # Test with an invalid URL
+            (
+                [
+                    "genai-perf",
+                    "profile",
+                    "--model",
+                    "test_model",
+                    "--service-kind",
+                    "triton",
+                    "--server-metrics-url",
+                    "invalid_url",
+                ],
+                "The URL passed for --server-metrics-url is invalid. "
+                "It must use 'http' or 'https', have a valid domain and port, "
+                "and contain '/metrics' in the path. The expected structure is: "
+                "<scheme>://<netloc>/<path>;<params>?<query>#<fragment>",
+            ),
+        ],
+    )
+    def test_server_metrics_url_invalid_args(
+        self, args_list, expected_error, monkeypatch, capsys
     ):
         monkeypatch.setattr("sys.argv", args_list)
 
-        if expected_error:
-            with pytest.raises(SystemExit) as excinfo:
-                parser.parse_args()
-            captured = capsys.readouterr()
-            assert expected_error in captured.err
-            assert excinfo.value.code != 0
-        else:
-            args, _ = parser.parse_args()
-            assert args.server_metrics_url == expected_url
+        with pytest.raises(SystemExit) as excinfo:
+            parser.parse_args()
+
+        captured = capsys.readouterr()
+        assert expected_error in captured.err
+        assert excinfo.value.code != 0
