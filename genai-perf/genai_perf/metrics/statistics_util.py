0431b79f339db4cecebe03f5487d267e2ea86d50
lkomali <lkomali@nvidia.com>
2024-09-03 10:59:41 -0700

    Fix pytest error

--- genai-perf/genai_perf/metrics/statistics_util.py
+++ genai-perf/genai_perf/metrics/statistics_util.py
@@ -26,7 +26,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from typing import List, Union, Tuple
+from typing import List, Tuple, Union
 
 import numpy as np
 
