b75144d3ac826a9b1f232362afa3bcca60433b55
Izzy Putterman <iputterman@nvidia.com>
2024-10-07 14:26:21 -0700

    Copy from GenAI branch

--- src/request_rate_manager.cc
+++ src/request_rate_manager.cc
@@ -138,8 +138,16 @@ RequestRateManager::GenerateSchedule(
     return;
   }
 
+  std::vector<float> scaled_schedule;
+  scaled_schedule.reserve(schedule.size());
+  if (schedule.size() > 0) {
+    for (const auto& value : schedule) {
+      scaled_schedule.push_back(value / static_cast<float>(request_rate));
+    }
+  }
+
   auto worker_schedules =
-      CreateWorkerSchedules(max_duration, distribution, schedule);
+      CreateWorkerSchedules(max_duration, distribution, scaled_schedule);
   GiveSchedulesToWorkers(worker_schedules);
 }
 
