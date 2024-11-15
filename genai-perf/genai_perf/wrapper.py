f6522711c9c0c10b4f7fdb9d4ab466de26827c7f
hongkuan <hongkuanz@nvidia.com>
2024-11-14 17:23:28 -0800

    typo

--- genai-perf/genai_perf/wrapper.py
+++ genai-perf/genai_perf/wrapper.py
@@ -108,7 +108,7 @@ class Profiler:
             "tokenizer",
             "tokenizer_trust_remote_code",
             "tokenizer_revision",
-            "triton-converter-set-end-id",
+            "triton_converter_set_end_id",
         ]
 
         utils.remove_file(args.profile_export_file)
