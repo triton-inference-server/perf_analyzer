c8b15a3fad709bb97b209dc7e697e49cbd498e09
nnshah1 <neelays@nvidia.com>
2024-12-06 00:22:05 -0800

    updating remove print

--- genai-perf/genai_perf/parser.py
+++ genai-perf/genai_perf/parser.py
@@ -167,7 +167,6 @@ def _check_conditional_args(
 
     # Endpoint and output format checks
     if args.service_kind == "openai":
-        print(args.endpoint_type)
         if args.endpoint_type is None:
             parser.error(
                 "The --endpoint-type option is required when using the 'openai' service-kind."
