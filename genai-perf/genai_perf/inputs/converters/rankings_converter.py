28af25a93a203cc473b59815d72014157e94f2cd
Elias Bermudez <dbermudez@nvidia.com>
2024-12-11 16:19:21 -0800

    Update rankings convertor - untested

--- genai-perf/genai_perf/inputs/converters/rankings_converter.py
+++ genai-perf/genai_perf/inputs/converters/rankings_converter.py
@@ -82,6 +82,7 @@ class RankingsConverter(BaseConverter):
                 }
 
             self._add_request_params(payload, config)
+            self._add_payload_params(payload, passage_entry.optional_data)
             request_body["data"].append({"payload": [payload]})
 
         return request_body
