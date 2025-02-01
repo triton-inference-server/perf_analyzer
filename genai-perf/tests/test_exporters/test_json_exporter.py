6fd0e30033ec1b51f77f3879b23362fc6cc7d217
braf <braf@nvidia.com>
2025-02-01 00:23:33 +0000

    JSON Exporter unit tests passing

--- genai-perf/tests/test_exporters/test_json_exporter.py
+++ genai-perf/tests/test_exporters/test_json_exporter.py
@@ -124,8 +124,18 @@ class TestJsonExporter:
             "std": 60,
         },
     }
-
-    expected_json_output = """
+    expected_json_input_config_keys = [
+        "model_names",
+        "analyze",
+        "endpoint",
+        "perf_analyzer",
+        "input",
+        "output",
+        "tokenizer",
+        "subcommand",
+        "verbose",
+    ]
+    expected_json_stats = """
       {
         "request_throughput": {
           "unit": "requests/sec",
@@ -212,57 +222,7 @@ class TestJsonExporter:
               "max": 68,
               "min": 69,
               "std": 60
-          },
-        "input_config": {
-          "model": ["gpt2_vllm"],
-          "formatted_model_name": "gpt2_vllm",
-          "model_selection_strategy": "round_robin",
-          "backend": "vllm",
-          "batch_size_image": 1,
-          "batch_size_text": 1,
-          "endpoint": null,
-          "endpoint_type": "kserve",
-          "service_kind": "triton",
-          "server_metrics_url": [],
-          "streaming": true,
-          "u": null,
-          "num_dataset_entries": 100,
-          "num_prefix_prompts": 0,
-          "output_tokens_mean": -1,
-          "output_tokens_mean_deterministic": false,
-          "output_tokens_stddev": 0,
-          "random_seed": 0,
-          "request_count": 0,
-          "synthetic_input_files": null,
-          "synthetic_input_tokens_mean": 550,
-          "synthetic_input_tokens_stddev": 0,
-          "prefix_prompt_length": 100,
-          "warmup_request_count": 0,
-          "image_width_mean": 100,
-          "image_width_stddev": 0,
-          "image_height_mean": 100,
-          "image_height_stddev": 0,
-          "image_format": null,
-          "concurrency": 1,
-          "measurement_interval": 10000,
-          "request_rate": null,
-          "stability_percentage": 999,
-          "generate_plots": false,
-          "profile_export_file": "artifacts/gpt2_vllm-triton-vllm-concurrency1/profile_export.json",
-          "artifact_dir": "artifacts/gpt2_vllm-triton-vllm-concurrency1",
-          "tokenizer": "hf-internal-testing/llama-tokenizer",
-          "tokenizer_revision": "main",
-          "tokenizer_trust_remote_code": false,
-          "verbose": false,
-          "goodput": null,
-          "header": null,
-          "subcommand": "profile",
-          "prompt_source": "synthetic",
-          "extra_inputs": {
-            "max_tokens": 256,
-            "ignore_eos": true
-          }
-        }
+            }
       }
     """
 
@@ -313,7 +273,13 @@ class TestJsonExporter:
             config=config,
         )
         json_exporter = JsonExporter(exporter_config)
-        assert json_exporter._stats_and_args == json.loads(self.expected_json_output)
+        input_config = json_exporter._stats_and_args["input_config"]
+        json_exporter._stats_and_args.pop("input_config")
+
+        for key in input_config.keys():
+            assert key in self.expected_json_input_config_keys
+
+        assert json_exporter._stats_and_args == json.loads(self.expected_json_stats)
         json_exporter.export()
         expected_filename = "profile_export_genai_perf.json"
         written_data = [
@@ -323,7 +289,7 @@ class TestJsonExporter:
         ]
 
         assert len(written_data) == 1
-        assert json.loads(written_data[0]) == json.loads(self.expected_json_output)
+        assert json.loads(written_data[0]) == json.loads(self.expected_json_stats)
 
     def test_generate_json_custom_export(
         self, monkeypatch, mock_read_write: pytest.MonkeyPatch
@@ -362,13 +328,12 @@ class TestJsonExporter:
         ]
 
         assert len(written_data) == 1
-        expected_json_output = json.loads(self.expected_json_output)
-        expected_json_output["input_config"][
-            "profile_export_file"
-        ] = expected_profile_filename
 
-        foo = json.loads(written_data[0])
-        assert json.loads(written_data[0]) == expected_json_output
+        actual_profile_filename = json.loads(written_data[0])["input_config"]["output"][
+            "profile_export_file"
+        ]
+
+        assert actual_profile_filename == expected_profile_filename
 
     def test_valid_goodput_json_output(
         self, monkeypatch, mock_read_write: pytest.MonkeyPatch
