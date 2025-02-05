701bfaeeb113d666e5d95114b505ea96c65add12
David Yastremsky <dyastremsky@nvidia.com>
2025-02-04 16:34:43 -0800

    Increase corpus size
    Remove unused logging statement

--- genai-perf/genai_perf/logging.py
+++ genai-perf/genai_perf/logging.py
@@ -1,4 +1,4 @@
-# Copyright 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2024-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -101,6 +101,11 @@ def init_logging() -> None:
                 "propagate": False,
             },
             "genai_perf.subcommand.common": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+                "propagate": False,
+            },
+            "genai_perf.inputs.retrievers.synthetic_prompt_generator": {
                 "handlers": ["console"],
                 "level": "DEBUG",
                 "propagate": False,
