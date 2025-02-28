f9fad61a09ee4553354439cddb8a064b561299b7
braf <braf@nvidia.com>
2025-02-28 21:51:41 +0000

    Fixing codeql issues

--- genai-perf/genai_perf/config/generate/perf_analyzer_config.py
+++ genai-perf/genai_perf/config/generate/perf_analyzer_config.py
@@ -20,7 +20,7 @@ from pathlib import Path
 from typing import Any, List, Optional
 
 from genai_perf.config.generate.search_parameter import SearchUsage
-from genai_perf.config.input.config_command import ConfigCommand, ConfigPerfAnalyzer
+from genai_perf.config.input.config_command import ConfigCommand
 from genai_perf.config.input.config_defaults import AnalyzeDefaults
 from genai_perf.constants import DEFAULT_GRPC_URL
 from genai_perf.exceptions import GenAIPerfException
