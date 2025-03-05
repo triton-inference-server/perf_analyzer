b4638b26d50df9df5f4af6781b7532974bf4a586
braf <braf@nvidia.com>
2025-03-05 15:53:48 +0000

    Removing session_concurrency from artifact stimulus (to match what CI test expects)

--- genai-perf/genai_perf/config/generate/perf_analyzer_config.py
+++ genai-perf/genai_perf/config/generate/perf_analyzer_config.py
@@ -193,8 +193,7 @@ class PerfAnalyzerConfig:
             request_rate = config.perf_analyzer.stimulus["request_rate"]
             stimulus = [f"request_rate{request_rate}"]
         elif "session_concurrency" in config.perf_analyzer.stimulus:
-            session_concurrency = config.perf_analyzer.stimulus["session_concurrency"]
-            stimulus = [f"session_concurrency{session_concurrency}"]
+            stimulus = [""]
         else:
             raise GenAIPerfException(f"Stimulus type not found in config")
 
