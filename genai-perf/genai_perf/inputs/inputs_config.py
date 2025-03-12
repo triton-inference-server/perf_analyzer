7c8f519d2de0606d02fada3f3e51cef03050007c
Hyunjae Woo <hwoo@nvidia.com>
2025-03-12 15:13:33 -0700

    address codeql

--- genai-perf/genai_perf/inputs/inputs_config.py
+++ genai-perf/genai_perf/inputs/inputs_config.py
@@ -28,7 +28,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
-from genai_perf.inputs.input_constants import *
+from genai_perf.inputs import input_constants as ic
 from genai_perf.inputs.retrievers import AudioFormat, ImageFormat
 from genai_perf.tokenizer import Tokenizer
 
@@ -50,13 +50,13 @@ class InputsConfig:
     add_stream: bool = False
 
     # The number of audio inputs per request
-    batch_size_audio: int = DEFAULT_BATCH_SIZE
+    batch_size_audio: int = ic.DEFAULT_BATCH_SIZE
 
     # The number of image inputs per request
-    batch_size_image: int = DEFAULT_BATCH_SIZE
+    batch_size_image: int = ic.DEFAULT_BATCH_SIZE
 
     # The number of text inputs per request
-    batch_size_text: int = DEFAULT_BATCH_SIZE
+    batch_size_text: int = ic.DEFAULT_BATCH_SIZE
 
     # If provided, append these inputs to every request
     extra_inputs: Dict[str, Any] = field(default_factory=dict)
@@ -71,33 +71,33 @@ class InputsConfig:
     payload_input_filename: Optional[Path] = Path("")
 
     # Specify how the input is received
-    input_type: PromptSource = PromptSource.SYNTHETIC
+    input_type: ic.PromptSource = ic.PromptSource.SYNTHETIC
 
     # Number of entries to gather
-    length: int = DEFAULT_LENGTH
+    length: int = ic.DEFAULT_LENGTH
 
     # The model name
     model_name: List[str] = field(default_factory=list)
 
     # The strategy to use when selecting models when multiple models are provided
-    model_selection_strategy: ModelSelectionStrategy = (
-        ModelSelectionStrategy.ROUND_ROBIN
+    model_selection_strategy: ic.ModelSelectionStrategy = (
+        ic.ModelSelectionStrategy.ROUND_ROBIN
     )
 
     # Specify the output format
-    output_format: OutputFormat = OutputFormat.TENSORRTLLM
+    output_format: ic.OutputFormat = ic.OutputFormat.TENSORRTLLM
 
     # If true, the output tokens will set the minimum and maximum tokens to be equivalent.
     output_tokens_deterministic: bool = False
 
     # The mean length of the output to generate. If not using fixed output lengths, this should be set to -1.
-    output_tokens_mean: int = DEFAULT_OUTPUT_TOKENS_MEAN
+    output_tokens_mean: int = ic.DEFAULT_OUTPUT_TOKENS_MEAN
 
     # The standard deviation of the length of the output to generate. This is only used if output_tokens_mean is provided.
-    output_tokens_stddev: int = DEFAULT_OUTPUT_TOKENS_STDDEV
+    output_tokens_stddev: int = ic.DEFAULT_OUTPUT_TOKENS_STDDEV
 
     # Offset from within the list to start gathering inputs
-    starting_index: int = DEFAULT_STARTING_INDEX
+    starting_index: int = ic.DEFAULT_STARTING_INDEX
 
     # The directory where all arifacts are saved
     output_dir: Path = Path("")
@@ -107,47 +107,47 @@ class InputsConfig:
     ########################################
 
     # The number of dataset entries to generate and use as the payload pool
-    num_dataset_entries: int = DEFAULT_NUM_DATASET_ENTRIES
+    num_dataset_entries: int = ic.DEFAULT_NUM_DATASET_ENTRIES
 
     # The mean length of the prompt to generate
-    prompt_tokens_mean: int = DEFAULT_PROMPT_TOKENS_MEAN
+    prompt_tokens_mean: int = ic.DEFAULT_PROMPT_TOKENS_MEAN
 
     # The standard deviation of the length of the prompt to generate
-    prompt_tokens_stddev: int = DEFAULT_PROMPT_TOKENS_STDDEV
+    prompt_tokens_stddev: int = ic.DEFAULT_PROMPT_TOKENS_STDDEV
 
     # Seed used to generate random values
-    random_seed: int = DEFAULT_RANDOM_SEED
+    random_seed: int = ic.DEFAULT_RANDOM_SEED
 
     # The number of prefix prompts to generate and pool from
-    num_prefix_prompts: int = DEFAULT_NUM_PREFIX_PROMPTS
+    num_prefix_prompts: int = ic.DEFAULT_NUM_PREFIX_PROMPTS
 
     # The length of the prefix prompts to generate
-    prefix_prompt_length: int = DEFAULT_PREFIX_PROMPT_LENGTH
+    prefix_prompt_length: int = ic.DEFAULT_PREFIX_PROMPT_LENGTH
 
     # The number of sessions to generate
-    num_sessions: int = DEFAULT_NUM_SESSIONS
+    num_sessions: int = ic.DEFAULT_NUM_SESSIONS
 
     # The mean number of turns per session
-    session_turns_mean: int = DEFAULT_SESSION_TURNS_MEAN
+    session_turns_mean: int = ic.DEFAULT_SESSION_TURNS_MEAN
 
     # The standard deviation of the number of turns per session
-    session_turns_stddev: int = DEFAULT_SESSION_TURNS_STDDEV
+    session_turns_stddev: int = ic.DEFAULT_SESSION_TURNS_STDDEV
 
     # The mean delay between turns in a session
-    session_turn_delay_mean: int = DEFAULT_SESSION_TURN_DELAY_MEAN_MS
+    session_turn_delay_mean: int = ic.DEFAULT_SESSION_TURN_DELAY_MEAN_MS
 
     # The standard deviation of the delay between turns in a session
-    session_turn_delay_stddev: int = DEFAULT_SESSION_TURN_DELAY_STDDEV_MS
+    session_turn_delay_stddev: int = ic.DEFAULT_SESSION_TURN_DELAY_STDDEV_MS
 
     #######################################
     # Synthetic Audio Generation Parameters
     #######################################
 
     # The mean length of the audio to generate in seconds.
-    audio_length_mean: float = DEFAULT_AUDIO_LENGTH_MEAN
+    audio_length_mean: float = ic.DEFAULT_AUDIO_LENGTH_MEAN
 
     # The standard deviation of the length of the audio to generate in seconds.
-    audio_length_stddev: float = DEFAULT_AUDIO_LENGTH_STDDEV
+    audio_length_stddev: float = ic.DEFAULT_AUDIO_LENGTH_STDDEV
 
     # The sampling rates of the audio to generate in kHz.
     audio_sample_rates: List[float] = field(default_factory=list)
@@ -156,7 +156,7 @@ class InputsConfig:
     audio_depths: List[int] = field(default_factory=list)
 
     # The number of channels of the audio to generate.
-    audio_num_channels: int = DEFAULT_AUDIO_NUM_CHANNELS
+    audio_num_channels: int = ic.DEFAULT_AUDIO_NUM_CHANNELS
 
     # The format of the audio to generate.
     audio_format: AudioFormat = AudioFormat.WAV
@@ -169,13 +169,13 @@ class InputsConfig:
     image_format: ImageFormat = ImageFormat.PNG
 
     # The mean height of images when generating synthetic image data.
-    image_height_mean: int = DEFAULT_IMAGE_HEIGHT_MEAN
+    image_height_mean: int = ic.DEFAULT_IMAGE_HEIGHT_MEAN
 
     # The standard deviation of height of images when generating synthetic image data.
-    image_height_stddev: int = DEFAULT_IMAGE_HEIGHT_STDDEV
+    image_height_stddev: int = ic.DEFAULT_IMAGE_HEIGHT_STDDEV
 
     # The mean width of images when generating synthetic image data.
-    image_width_mean: int = DEFAULT_IMAGE_WIDTH_MEAN
+    image_width_mean: int = ic.DEFAULT_IMAGE_WIDTH_MEAN
 
     # The standard deviation of width of images when generating synthetic image data.
-    image_width_stddev: int = DEFAULT_IMAGE_WIDTH_STDDEV
+    image_width_stddev: int = ic.DEFAULT_IMAGE_WIDTH_STDDEV
