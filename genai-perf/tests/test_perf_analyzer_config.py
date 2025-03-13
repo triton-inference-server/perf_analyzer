b5d84b8e5f14c658aa6070424b6694da70407e6e
braf <braf@nvidia.com>
2025-03-13 17:27:59 +0000

    Add more unit testing to PA config generation

--- genai-perf/tests/test_perf_analyzer_config.py
+++ genai-perf/tests/test_perf_analyzer_config.py
@@ -28,6 +28,8 @@ from genai_perf.config.generate.perf_analyzer_config import (
 )
 from genai_perf.config.generate.search_parameters import SearchUsage
 from genai_perf.config.input.config_command import ConfigCommand, ConfigPerfAnalyzer
+from genai_perf.constants import DEFAULT_GRPC_URL
+from genai_perf.inputs.input_constants import OutputFormat, PromptSource
 
 
 class TestPerfAnalyzerConfig(unittest.TestCase):
@@ -210,6 +212,313 @@ class TestPerfAnalyzerConfig(unittest.TestCase):
 
         # Catchall in case something new is added
         self.assertEqual(pa_config_from_checkpoint, self._default_perf_analyzer_config)
+
+    ###########################################################################
+    # Test _add_required_args
+    ###########################################################################
+    def test_add_required_args_with_dynamic_grpc(self):
+        """
+        Test that _add_required_args returns the correct arguments
+        when service_kind is 'dynamic_grpc'
+        """
+        self._config.endpoint.service_kind = "dynamic_grpc"
+        expected_args = [f"{self._config.perf_analyzer.path}"]
+
+        actual_args = self._default_perf_analyzer_config._add_required_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_required_args_with_non_dynamic_grpc(self):
+        """
+        Test that _add_required_args returns the correct arguments
+        when service_kind is not 'dynamic_grpc'
+        """
+        self._config.endpoint.service_kind = "triton"
+        expected_args = [
+            f"{self._config.perf_analyzer.path}",
+            "-m",
+            "test_model",
+            "--async",
+        ]
+
+        actual_args = self._default_perf_analyzer_config._add_required_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    ###########################################################################
+    # Test _add_perf_analyzer_args
+    ###########################################################################
+    def test_add_perf_analyzer_args_with_payload_prompt_source(self):
+        """
+        Test that _add_perf_analyzer_args returns an empty list
+        when prompt_source is PAYLOAD
+        """
+        self._config.input.prompt_source = PromptSource.PAYLOAD
+        expected_args = []
+
+        actual_args = self._default_perf_analyzer_config._add_perf_analyzer_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_perf_analyzer_args_with_non_payload_prompt_source(self):
+        """
+        Test that _add_perf_analyzer_args returns the correct arguments
+        when prompt_source is not PAYLOAD
+        """
+        self._config.input.prompt_source = PromptSource.FILE
+        self._config.perf_analyzer.stability_percentage = 999
+        self._config.perf_analyzer.measurement_interval = 10000
+        self._config.perf_analyzer.request_count.num = 0
+        self._config.perf_analyzer.request_count.warmup = 0
+
+        expected_args = [
+            "--stability-percentage",
+            "999",
+            "--measurement-interval",
+            "10000",
+            "--request-count",
+            "0",
+            "--warmup-request-count",
+            "0",
+        ]
+
+        actual_args = self._default_perf_analyzer_config._add_perf_analyzer_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    ###########################################################################
+    # Test _add_protocol_args
+    ###########################################################################
+    def test_add_protocol_args_with_triton(self):
+        """
+        Test that _add_protocol_args returns the correct arguments
+        when service_kind is 'triton'
+        """
+        self._config.endpoint.service_kind = "triton"
+        self._config.endpoint.backend = OutputFormat.TENSORRTLLM
+        self._config.endpoint.get_field("url").is_set_by_user = False
+
+        expected_args = [
+            "-i",
+            "grpc",
+            "--streaming",
+            "-u",
+            f"{DEFAULT_GRPC_URL}",
+            "--shape",
+            "max_tokens:1",
+            "--shape",
+            "text_input:1",
+        ]
+
+        actual_args = self._default_perf_analyzer_config._add_protocol_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_protocol_args_with_openai(self):
+        """
+        Test that _add_protocol_args returns the correct arguments
+        when service_kind is 'openai'
+        """
+        self._config.endpoint.service_kind = "openai"
+
+        expected_args = ["-i", "http"]
+
+        actual_args = self._default_perf_analyzer_config._add_protocol_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_protocol_args_with_dynamic_grpc(self):
+        """
+        Test that _add_protocol_args returns the correct arguments
+        when service_kind is 'dynamic_grpc'
+        """
+        self._config.endpoint.service_kind = "dynamic_grpc"
+        self._config.endpoint.get_field("url").is_set_by_user = False
+
+        expected_args = ["-u", f"{DEFAULT_GRPC_URL}"]
+
+        actual_args = self._default_perf_analyzer_config._add_protocol_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    ###########################################################################
+    # Test _add_inference_load_args
+    ###########################################################################
+    def test_add_inference_load_args_with_no_parameters_and_set_stimulus(self):
+        """
+        Test that _add_inference_load_args returns the correct arguments
+        when there are no parameters and stimulus is set by user
+        """
+        self._default_perf_analyzer_config._parameters = {}
+        self._config.perf_analyzer.get_field("stimulus").is_set_by_user = True
+        self._config.perf_analyzer.stimulus = {"concurrency": 10}
+
+        expected_args = ["--concurrency-range", "10"]
+
+        actual_args = self._default_perf_analyzer_config._add_inference_load_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_inference_load_args_with_no_parameters_and_request_rate(self):
+        """
+        Test that _add_inference_load_args returns the correct arguments
+        when there are no parameters and request_rate is set by user
+        """
+        self._default_perf_analyzer_config._parameters = {}
+        self._config.perf_analyzer.get_field("stimulus").is_set_by_user = True
+        self._config.perf_analyzer.stimulus = {"request_rate": 5}
+
+        expected_args = ["--request-rate-range", "5"]
+
+        actual_args = self._default_perf_analyzer_config._add_inference_load_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_inference_load_args_with_no_parameters_and_session_concurrency(self):
+        """
+        Test that _add_inference_load_args returns the correct arguments
+        when there are no parameters and session_concurrency is set by user
+        """
+        self._default_perf_analyzer_config._parameters = {}
+        self._config.perf_analyzer.get_field("stimulus").is_set_by_user = True
+        self._config.perf_analyzer.stimulus = {"session_concurrency": 3}
+
+        expected_args = ["--session-concurrency", "3"]
+
+        actual_args = self._default_perf_analyzer_config._add_inference_load_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_inference_load_args_with_parameters(self):
+        """
+        Test that _add_inference_load_args returns the correct arguments
+        when parameters are set
+        """
+        self._default_perf_analyzer_config._parameters = {
+            "concurrency": 10,
+            "request_rate": 5,
+            "runtime_batch_size": 2,
+        }
+
+        expected_args = [
+            "--concurrency-range",
+            "10",
+            "--request-rate-range",
+            "5",
+            "-b",
+            "2",
+        ]
+
+        actual_args = self._default_perf_analyzer_config._add_inference_load_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    ###########################################################################
+    # Test _add_prompt_source_args
+    ###########################################################################
+    def test_add_prompt_source_args_with_payload_and_no_session_concurrency(self):
+        """
+        Test that _add_prompt_source_args returns the correct arguments
+        when prompt_source is PAYLOAD and session_concurrency is not in stimulus
+        """
+        self._config.input.prompt_source = PromptSource.PAYLOAD
+        self._config.perf_analyzer.stimulus = {}
+
+        expected_args = ["--fixed-schedule"]
+
+        actual_args = self._default_perf_analyzer_config._add_prompt_source_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_prompt_source_args_with_payload_and_session_concurrency(self):
+        """
+        Test that _add_prompt_source_args returns an empty list
+        when prompt_source is PAYLOAD and session_concurrency is in stimulus
+        """
+        self._config.input.prompt_source = PromptSource.PAYLOAD
+        self._config.perf_analyzer.stimulus = {"session_concurrency": 3}
+
+        expected_args = []
+
+        actual_args = self._default_perf_analyzer_config._add_prompt_source_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_prompt_source_args_with_non_payload(self):
+        """
+        Test that _add_prompt_source_args returns an empty list
+        when prompt_source is not PAYLOAD
+        """
+        self._config.input.prompt_source = PromptSource.FILE
+
+        expected_args = []
+
+        actual_args = self._default_perf_analyzer_config._add_prompt_source_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    ###########################################################################
+    # Test _add_endpoint_args
+    ###########################################################################
+    def test_add_endpoint_args_with_custom_endpoint(self):
+        """
+        Test that _add_endpoint_args returns the correct arguments
+        when custom endpoint is set
+        """
+        self._config.endpoint.service_kind = "triton"
+        self._config.endpoint.custom = "custom_endpoint"
+
+        expected_args = ["--service-kind", "triton", "--endpoint", "custom_endpoint"]
+
+        actual_args = self._default_perf_analyzer_config._add_endpoint_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
+
+    def test_add_endpoint_args_without_custom_endpoint(self):
+        """
+        Test that _add_endpoint_args returns the correct arguments
+        when custom endpoint is not set
+        """
+        self._config.endpoint.service_kind = "triton"
+        self._config.endpoint.custom = None
+
+        expected_args = ["--service-kind", "triton"]
+
+        actual_args = self._default_perf_analyzer_config._add_endpoint_args(
+            self._config
+        )
+
+        self.assertEqual(expected_args, actual_args)
 
 
 if __name__ == "__main__":
