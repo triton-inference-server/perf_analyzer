399b5aa56f1f735e86622119aceb06a07dbcdd52
Ray Huang <bjhuang@cs.nycu.edu.tw>
2025-08-04 15:22:47 +0800

    fix: compatible with empty header name config

--- genai-perf/genai_perf/config/input/config_command.py
+++ genai-perf/genai_perf/config/input/config_command.py
@@ -110,7 +110,13 @@ class ConfigCommand(BaseConfig):
         skip_inferencing_and_checking: bool = False,
     ) -> None:
         if user_config:
-            for key, value in user_config.items():
+            # Empty header name checks
+            if user_config.get("model_name") is not None:
+                config_items = user_config.items()
+            else:
+                config_items = user_config[list(user_config.keys())[0]].items()
+
+            for key, value in config_items:
                 if key == "model_name" or key == "model_names":
                     self._parse_model_names(value)
                 elif key == "analyze":
